a3458f1f9a1c2ae1161f39f878d51a58fb57ffcf
williamdothardy33 <william.hardy33@yahoo.com>
2024-06-08 18:21:07 -0400

    finished impl of trie

--- trie.py
+++ trie.py
@@ -9,7 +9,7 @@ def trie(data):
         def __init__(self):
             self.root = trie_node()
 
-        def search(self, search_term):
+        def search(self, search_term, node):
             def search_recur(search_term, search_length, current_node, search_pointer):
                 if search_pointer >= search_length:
                     return current_node
@@ -35,8 +35,24 @@ def trie(data):
                 return current_node
             search_length = len(search_term)
             
-            #return search_recur(search_term, search_length, self.root, 0)
-            return search_iter(search_term, self.root)
+            #return search_recur(search_term, search_length, node, 0)
+            return search_iter(search_term, node)
+        
+        def longest_prefix(self, search_term):
+            search_pointer = 0
+            search_length = len(search_term)
+            current = self.root
+            while search_pointer < search_length:
+                search_part = search_term[search_pointer]
+                next = self.search(search_part, current)
+                if next is not None:
+                    current = next
+                    search_pointer += 1
+            if search_pointer > 0:
+                return current
+            
+    
+        
 
         def insert(self, data):
             def insert_recur(data, data_length, data_pointer, current_node):
@@ -74,7 +90,68 @@ def trie(data):
             #insert_recur(data, data_length, 0, self.root)
             insert_iter(data, self.root)
 
-        #def words_from(self, node):
+        def contents_from(self, node):
+            def data_from_recur(prefix, prefix_node, result, results):
+                if prefix != '*':
+                    for next_prefix, next_node in prefix_node.children.items():
+                        data_from_recur(next_prefix, next_node, result + prefix, results)        
+                else:
+                    results.append(result)
+
+            results = []
+
+            for prefix, suffixes in node.children.items():
+                result = ''
+                data_from_recur(prefix, suffixes, result, results)
+
+            return results
+        
+        def auto_suggest(self, search_term):
+            results = []
+            next_node = self.search(search_term, self.root)
+            if next_node is not None:
+                for result in self.contents_from(next_node):
+                    results.append(search_term + result)
+            return results
+        
+        def auto_suggest_fix(self, search_term):
+            search_pointer = 0
+            search_length = len(search_term)
+            current = self.root
+            prefix = ''
+            while search_pointer < search_length:
+                search_part = search_term[search_pointer]
+                next = self.search(search_part, current)
+                if next is not None:
+                    prefix += search_part
+                    current = next
+                    search_pointer += 1
+                else:
+                    break
+            
+            if search_pointer == search_length:
+                return [search_term]
+            else:
+                if search_pointer > 0:
+                    results = []
+                    for result in self.contents_from(current):
+                        results.append(prefix + result)
+                    return results
+        
+        def trie_keys(self, node):
+            def trie_keys_recur(node, results):
+                if node is not None:
+                    for key, node in node.children.items():
+                        results.append(key)
+                        trie_keys_recur(node, results)
+
+            results = []
+            trie_keys_recur(node, results)
+            return results
+
+
+
+
 
 
 
@@ -85,11 +162,33 @@ def trie(data):
     return result
 
 def test_tree():
-    data = 'catnip'
-    a_trie = trie(data)
-    search_result = a_trie.search('cat')
-    print(f'result of search is: {search_result.children.keys()}')
-    print(f'words in trie are: {a_trie.words_from(a_trie.root)}')
+    data1 = 'calorie'
+    data2 = 'cantrip'
+    data3 = 'canter'
+    data4 = 'cancel'
+    data5 = 'cat'
+    data6 = 'catnip'
+    data7 = 'catnap'
+    
+
+    a_trie = trie(data1)
+    a_trie.insert(data2)
+    a_trie.insert(data3)
+    a_trie.insert(data4)
+    a_trie.insert(data5)
+    a_trie.insert(data6)
+    a_trie.insert(data7)
+
+
+    #search_result = a_trie.search('catn'. a_trie.root)
+    #print(f'result of search is: {search_result.children.keys()}')
+    #print(f'words in trie are: {a_trie.contents_from(a_trie.root)}')
+    result = a_trie.auto_suggest('cat')
+    print(f'result of auto_suggest for \'cat\' is: {result}')
+    #keys = a_trie.trie_keys(a_trie.root)
+    #print(f'keys in a_trie are: {keys}')
+    result1 = a_trie.auto_suggest_fix('candida')
+    print(f'result of auto_suggest_fix for \'ca\' is: {result1} ')
 
 test_tree()
                     
