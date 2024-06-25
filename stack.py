5df3ca5ab8927a530f37ca2c7d37043b6c73a614
williamdothardy33 <william.hardy33@yahoo.com>
2024-06-25 12:44:01 -0400

    added some convenience methods

--- stack.py
+++ stack.py
@@ -1,19 +1,34 @@
-class stack(object):
-    underlying = []
-
-    def __init__(self) -> None:
-        pass
-    def push(self, e):
-        self.underlying.append(e)
-
-    def pop(self):
-        return self.underlying.pop()
-
-    def is_empty(self):
-        if len(self.underlying) == 0:
-            return True
-        else:
-            return False
-
-    def show(self):
-        return self.underlying
+def stack():
+    class stack(object):
+        underlying = []
+    
+        def __init__(self) -> None:
+            pass
+        def push(self, e):
+            self.underlying.append(e)
+    
+        def pop(self):
+            return self.underlying.pop()
+    
+        def is_empty(self):
+            if len(self.underlying) == 0:
+                return True
+            else:
+                return False
+            
+        def peek(self):
+            return self.underlying[len(self.underlying) - 1]
+        
+        def to_array(self):
+            result = []
+            pointer = 0
+            while pointer < len(self.underlying):
+                result.append(self.underlying[pointer])
+                pointer += 1
+    
+            return result
+    
+        def show(self):
+            return self.to_array()
+    result = stack()
+    return result
